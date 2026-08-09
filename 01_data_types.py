d155b819c291549be32bb435c0b57baf266eb80f
Aether ZvN <zexvronofficial@gmail.com>
2026-08-09 17:22:33 +0700

    Python basic : Data types

---
+++ 01_data_types.py
@@ -0,0 +1,27 @@
+# Numbers can do math
+total = 10 + 5
+print(total)
+
+# Strings combine differently
+name = "Deni" + " " + "Septiawan"
+print(name)
+
+# Cant mix without converting
+# age = "14" + 12 # ERROR!
+age = int("14") + 12
+print(age)
+
+# == TYPE() ==
+
+# Check different types
+print("Type data = ", type(42))
+print("Type data =", type(3.14))
+print("Type data =", type("Hello World"))
+print("Type data =", type(True))
+
+
+# Check Variables
+varNumber = 100
+varString = "Deni"
+print("Type data varNumber =", type(varNumber))
+print("Type data varString =", type(varString))
