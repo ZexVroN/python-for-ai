b1b8c0e69e3bba92a362c97b36d23e92cffb2678
Aether ZvN <zexvronofficial@gmail.com>
2026-08-20 22:31:09 +0700

    Learn Python return

---
+++ 15_return.py
@@ -0,0 +1,15 @@
+def addition(x, y):
+    return x + y
+
+
+nilai = addition(10, 20)
+nilai += 30  # 60
+nilai = nilai + 30  # 90
+print(nilai)
+
+
+def test():
+    return 10
+
+
+print(test())  # 10
